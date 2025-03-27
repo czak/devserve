4973b3b58469ff0681a3eac5a7c00727baf644d0
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-03-27 12:13:06 +0100

    Initial file server with script injection

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+const eventScript = `<script>
+new EventSource('/events').addEventListener('change', () => location.reload())
+</script>`
+
+func main() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal("Failed to get cwd")
+	}
+
+	http.HandleFunc("/", serveFiles(cwd))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func serveFiles(dir string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fp := resolveFilepath(dir, r.URL.Path)
+
+		if filepath.Ext(fp) == ".html" {
+			serveHtml(w, r, fp)
+		} else {
+			http.ServeFile(w, r, fp)
+		}
+	}
+}
+
+// Get local file path of the requested file.
+// If request is for a directory, append "index.html".
+//
+// Note the path is not guaranteed to be for an existing file.
+func resolveFilepath(dir, path string) string {
+	fp := filepath.Join(dir, path)
+
+	if path[len(path)-1] == '/' {
+		ip := filepath.Join(fp, "index.html")
+		if _, err := os.Stat(ip); err == nil {
+			fp = ip
+		}
+	}
+
+	return fp
+}
+
+// Serve a HTML file with injected <script>
+func serveHtml(w http.ResponseWriter, r *http.Request, fp string) {
+	html, err := os.ReadFile(fp)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	html = bytes.Replace(html, []byte("</body>"), []byte(eventScript+"</body>"), 1)
+
+	w.Write(html)
+}
