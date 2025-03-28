cc6d57c330c82e87c390c216523ae24260691e9e
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-03-28 12:02:54 +0100

    Hot reload for CSS
    Source: https://esbuild.github.io/api/#live-reload

--- event.js
+++ event.js
@@ -1,1 +1,17 @@
-new EventSource('/events').addEventListener('change', () => location.reload())
+new EventSource('/events').addEventListener('change', e => {
+  const path = '/' + e.data
+
+  for (const link of document.getElementsByTagName("link")) {
+    const url = new URL(link.href)
+
+    if (url.host === location.host && url.pathname === path) {
+      const next = link.cloneNode()
+      next.href = path + '?' + Math.random().toString(36).slice(2)
+      next.onload = () => link.remove()
+      link.parentNode.insertBefore(next, link.nextSibling)
+      return
+    }
+  }
+
+  location.reload()
+})
